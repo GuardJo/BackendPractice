28d44f65f6b43c1ed097a05c2bcbab75b781a97b
kyeongho <dagh1218@naver.com>
2022-10-05 00:56:15 +0900

    테스트 코드 작성 연습
    - gradle 사용
    - assertJ 라이브러리 사용
    - junit-params 라이브러리 사용
    - passay 라이브러리 사용 (암호 생성 라이브러리)

---
+++ oop-practice/src/main/java/org/example/RandomPasswordGenerator.java
@@ -0,0 +1,45 @@
+package org.example;
+
+import org.passay.CharacterData;
+import org.passay.CharacterRule;
+import org.passay.EnglishCharacterData;
+import org.passay.PasswordGenerator;
+
+public class RandomPasswordGenerator implements org.example.PasswordGenerator {
+    public static final String ALLOWED_SPL_CHARACTERS = "!@#$%^&*()_+";
+    public static final String ERROR_CODE = "ERRONEOUS_SPECTIAL_CHARS";
+
+    public String generatePassword() {
+        PasswordGenerator passwordGenerator = new PasswordGenerator();
+
+        CharacterData lowerCaseChars = EnglishCharacterData.LowerCase;
+        CharacterRule lowerCaseRule = new CharacterRule(lowerCaseChars);
+        lowerCaseRule.setNumberOfCharacters(2);
+
+        CharacterData upperCaseChars = EnglishCharacterData.UpperCase;
+        CharacterRule upperCaseRule = new CharacterRule(upperCaseChars);
+        upperCaseRule.setNumberOfCharacters(2);
+
+        CharacterData digitChars = EnglishCharacterData.Digit;
+        CharacterRule digitRule = new CharacterRule(digitChars);
+        digitRule.setNumberOfCharacters(2);
+
+        CharacterData specialChars = new CharacterData() {
+            @Override
+            public String getErrorCode() {
+                return ERROR_CODE;
+            }
+
+            @Override
+            public String getCharacters() {
+                return ALLOWED_SPL_CHARACTERS;
+            }
+        };
+
+        CharacterRule specialRule = new CharacterRule(specialChars);
+        specialRule.setNumberOfCharacters(2);
+
+        // 0 ~ 12
+        return passwordGenerator.generatePassword((int) (Math.random() * 13), specialRule, lowerCaseRule, upperCaseRule, digitRule);
+    }
+}
