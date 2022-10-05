1cff8bd2061c63d3e7c40cb7a5deb2801d88750e
kyeongho <dagh1218@naver.com>
2022-10-06 00:44:01 +0900

    기존 사칙연산 구조를 enum에서 처리하도록 리팩토링
    - 기존 사칙연산 구조를 enum에서 처리하도록 리팩토링

--- oop-practice/src/main/java/org/example/password/generator/calculator/Calculator.java
+++ oop-practice/src/main/java/org/example/password/generator/calculator/Calculator.java
@@ -2,18 +2,6 @@ package org.example.password.generator.calculator;
 
 public class Calculator {
     public static int calculate(int operand1, String operator, int operand2) {
-        if (operator.equals("+")) {
-            return operand1 + operand2;
-        }
-        else if (operator.equals("-")) {
-            return operand1 - operand2;
-        }
-        else if (operator.equals("*")) {
-            return operand1 * operand2;
-        }
-        else if (operator.equals("/")) {
-            return operand1 / operand2;
-        }
-        return operand1 + operand2;
+        return ArithMeticOperator.calculate(operand1, operator, operand2);
     }
 }
