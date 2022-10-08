4554d50bfd8dce46fc5d636a470dc2249d42e0cf
kyeongho <dagh1218@naver.com>
2022-10-08 15:14:13 +0900

    계산기 웹서비스 프로젝트 생성
    - 이전에 구현한 계산기 활용
    - 요구사항 정의

---
+++ was-practice/src/main/java/org/example/was/WebApplicationServer.java
@@ -0,0 +1,9 @@
+package org.example.was;
+
+/**
+ * 요구사항
+ * GET /caluate?operand1=1&operator=+&operand2=2
+ * 위와 같은 형식의 URL 열기
+ */
+public class WebApplicationServer {
+}
---
+++ was-practice/src/main/java/org/example/was/calculator/Calculator.java
@@ -0,0 +1,20 @@
+package org.example.was.calculator;
+
+import org.example.calculator.operator.*;
+import org.example.was.calculator.operator.*;
+
+import java.util.List;
+
+public class Calculator {
+    private final static List<NewArithmeticOperator> arithmeticOperators = List.of(
+            new AdditionOperator(), new SubtractionOperator(), new MultiplicationOperator(), new DivisionOperator()
+    );
+
+    public static int calculate(int operand1, String operator, int operand2) {
+        return arithmeticOperators.stream()
+                .filter((arithmeticOperator) -> arithmeticOperator.isCurrectOperator(operator))
+                .map((arithmeticOperator) -> arithmeticOperator.calculate(new PositiveNumber(operand1), new PositiveNumber(operand2)))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("Wrong Operator"));
+    }
+}
