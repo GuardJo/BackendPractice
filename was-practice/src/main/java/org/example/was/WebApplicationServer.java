6a16b82867b6565544740855661d3e94e390f2af
kyeongho <dagh1218@naver.com>
2022-10-10 02:16:54 +0900

    ThreadPool을 활용하여 Thread 메모리 관리
    - ThreadPool을 활용하여 Thread 메모리 관리

--- was-practice/src/main/java/org/example/was/WebApplicationServer.java
+++ was-practice/src/main/java/org/example/was/WebApplicationServer.java
@@ -7,6 +7,8 @@ import org.slf4j.LoggerFactory;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * 요구사항
@@ -16,6 +18,7 @@ import java.net.Socket;
 public class WebApplicationServer {
     private final int port;
     private static final Logger logger = LoggerFactory.getLogger(WebApplicationServer.class);
+    private final ExecutorService executorService = Executors.newFixedThreadPool(10);
 
     public WebApplicationServer(int port) {
         this.port = port;
@@ -28,11 +31,11 @@ public class WebApplicationServer {
             Socket clientSocket;
             logger.info("waiting client...");
 
-            // 사용자 요청이 올 때마다 Thread 생성
+            // Thread Pool을 사용하여 자원 아끼기
             while ((clientSocket = serverSocket.accept()) != null) {
                 logger.info("Client Connected!");
 
-                new Thread(new RequestHandler(clientSocket)).start();
+                executorService.execute(new RequestHandler(clientSocket));
             }
         }
     }
