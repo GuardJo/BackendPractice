f7d6002893e7d3cfbaeeef396a425d2d02a48cfe
kyeongho <dagh1218@naver.com>
2022-10-12 00:29:26 +0900

    UserDao, User 객체 생성 및 뼈대 구축
    - UserDao, User 객체 생성 및 뼈대 구축
    - User 스키마 생성
    - UserDao create 테스트 작성

---
+++ jdbc-practice/src/main/java/org/example/jdbc/UserDao.java
@@ -0,0 +1,11 @@
+package org.example.jdbc;
+
+public class UserDao {
+    public void create(User user) {
+
+    }
+
+    public User findById(String userId) {
+        return null;
+    }
+}
--- jdbc-practice/src/test/java/org/example/jdbc/UserDaoTest.java
+++ jdbc-practice/src/test/java/org/example/jdbc/UserDaoTest.java
@@ -1,9 +1,13 @@
 package org.example.jdbc;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.datasource.init.DatabasePopulatorUtils;
 import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class UserDaoTest {
     @BeforeEach
@@ -11,5 +15,18 @@ public class UserDaoTest {
         ResourceDatabasePopulator resourceDatabasePopulator = new ResourceDatabasePopulator();
         resourceDatabasePopulator.addScript(new ClassPathResource("db_schema.sql"));
         DatabasePopulatorUtils.execute(resourceDatabasePopulator, ConnectionHandler.getDataSource());
+    }
+
+    @DisplayName("UserDao 생성 및 정상 생성 확인 테스트")
+    @Test
+    void testCreate() {
+        UserDao userDao = new UserDao();
+        User createUser = new User("userId", "password", "name", "email");
+
+        userDao.create(createUser);
+
+        User findUser = userDao.findById("userId");
+
+        assertThat(createUser).isEqualTo(findUser);
     }
 }
