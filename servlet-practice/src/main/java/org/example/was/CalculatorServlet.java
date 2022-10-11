1f87a44b76be8f2b8ba9f99403ed257e63913501
kyeongho <dagh1218@naver.com>
2022-10-11 23:37:14 +0900

    Servlet 인터페이스를 활용한 CustomServlet 구현
    - Servlet 인터페이스를 활용한 CustomServlet 구현

--- servlet-practice/src/main/java/org/example/was/CalculatorServlet.java
+++ servlet-practice/src/main/java/org/example/was/CalculatorServlet.java
@@ -1,29 +1,44 @@
 package org.example.was;
 
+import org.example.was.calculator.Calculator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebServlet;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet("/calculate")
 public class CalculatorServlet implements Servlet {
     private static final Logger log = LoggerFactory.getLogger(CalculatorServlet.class);
+    private ServletConfig servletConfig;
 
     @Override
     public void init(ServletConfig config) throws ServletException {
         log.info("servlet init");
+
+        this.servletConfig = config;
     }
 
     @Override
     public ServletConfig getServletConfig() {
-        return null;
+        return this.servletConfig;
     }
 
     @Override
     public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
         log.info("servlet service");
+
+        int operand1 = Integer.parseInt(req.getParameter("operand1"));
+        String operator = req.getParameter("operator");
+        int operand2 = Integer.parseInt(req.getParameter("operand2"));
+
+        int result = Calculator.calculate(operand1, operator, operand2);
+
+        PrintWriter printWriter = res.getWriter();
+
+        printWriter.println(result);
     }
 
     @Override
