fd2976bbfe6ddf60ec851cf9592668736d8c517a
kyeongho <dagh1218@naver.com>
2022-10-13 23:59:09 +0900

    DB 커넥션 풀 설정 및 구조 리팩토링
    - HikariCP 사용
    - 일부 DB 커넥션 설정 요소 분리

--- jdbc-practice/src/main/java/org/example/jdbc/ConnectionHandler.java
+++ jdbc-practice/src/main/java/org/example/jdbc/ConnectionHandler.java
@@ -3,16 +3,27 @@ package org.example.jdbc;
 import com.zaxxer.hikari.HikariDataSource;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 public class ConnectionHandler {
+    private static DataSource dataSource;
+
+    static {
+        HikariDataSource hikariDataSource = new HikariDataSource();
+        hikariDataSource.setDriverClassName(DBConnectionConstant.DB_DRIVER_NAME);
+        hikariDataSource.setJdbcUrl(DBConnectionConstant.DB_URL);
+        hikariDataSource.setUsername(DBConnectionConstant.DB_USER_NAME);
+        hikariDataSource.setPassword(DBConnectionConstant.DB_PASSWORD);
+        
+        dataSource = hikariDataSource;
+    }
+
+    public Connection getConnection() throws SQLException {
+        return dataSource.getConnection();
+    }
 
     public static DataSource getDataSource() {
-        HikariDataSource hikariDataSource = new HikariDataSource();
-        hikariDataSource.setDriverClassName("org.h2.Driver");
-        hikariDataSource.setJdbcUrl("jdbc:h2:mem://localhost/~/jdbc-practice;MODE=MySQL;DB_CLOSE_DELAY=-1");
-        hikariDataSource.setUsername("Kyeongho");
-        hikariDataSource.setPassword("1234");
-
-        return hikariDataSource;
+        return dataSource;
     }
 }
---
+++ jdbc-practice/src/main/java/org/example/jdbc/DBConnectionConstant.java
@@ -0,0 +1,8 @@
+package org.example.jdbc;
+
+public class DBConnectionConstant {
+    public static final String DB_DRIVER_NAME = "org.h2.Driver";
+    public static final String DB_URL = "jdbc:h2:mem://localhost/~/jdbc-practice;MODE=MySQL;DB_CLOSE_DELAY=-1";
+    public static final String DB_USER_NAME = "Kyeongho";
+    public static final String DB_PASSWORD = "1234";
+}
