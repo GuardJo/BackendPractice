2de5e2dc2d371d64a4949377fe35b49ab299bfca
kyeongho <dagh1218@naver.com>
2022-10-14 00:42:32 +0900

    create(), findById() 메소드 리팩토링
    - 자체 구현한 RowMapper, PreparedStatemntSetter, JdbcTemplate 사용

--- jdbc-practice/src/main/java/org/example/jdbc/UserDao.java
+++ jdbc-practice/src/main/java/org/example/jdbc/UserDao.java
@@ -4,60 +4,41 @@ import java.sql.*;
 
 public class UserDao {
     public void create(User user) throws SQLException {
-        Connection connection = null;
-        PreparedStatement preparedStatement = null;
         String sql = "INSERT INTO USERS VALUES(?, ?, ?, ?)";
 
-        try {
-            connection = ConnectionHandler.getConnection();
-            preparedStatement = connection.prepareStatement(sql);
-            preparedStatement.setString(1, user.getUserId());
-            preparedStatement.setString(2, user.getPassword());
-            preparedStatement.setString(3, user.getName());
-            preparedStatement.setString(4, user.getEmail());
+        SimpleJdbcTemplate simpleJdbcTemplate = new SimpleJdbcTemplate();
+        simpleJdbcTemplate.executeUpdate(user, sql, new SimplePreparedStatementSetter() {
+            @Override
+            public PreparedStatement setter(PreparedStatement preparedStatement) throws SQLException {
+                preparedStatement.setString(1, user.getUserId());
+                preparedStatement.setString(2, user.getPassword());
+                preparedStatement.setString(3, user.getName());
+                preparedStatement.setString(4, user.getEmail());
 
-            preparedStatement.executeUpdate();
-        } finally {
-            if (preparedStatement != null) {
-                preparedStatement.close();
+                return preparedStatement;
             }
-
-            if (connection != null) {
-                connection.close();
-            }
-        }
+        });
     }
 
     public User findById(String userId) throws SQLException {
-        Connection connection = null;
-        PreparedStatement preparedStatement = null;
-        ResultSet resultSet = null;
+        SimpleJdbcTemplate simpleJdbcTemplate = new SimpleJdbcTemplate();
         String sql = "SELECT userId, password, name, email FROM USERS WHERE userId=?";
         User user = null;
 
-        try {
-            connection = ConnectionHandler.getConnection();
-            preparedStatement = connection.prepareStatement(sql);
-            preparedStatement.setString(1, userId);
-            resultSet = preparedStatement.executeQuery();
+        user = simpleJdbcTemplate.executeQuery(userId, sql, new SimplePreparedStatementSetter() {
+            @Override
+            public PreparedStatement setter(PreparedStatement preparedStatement) throws SQLException {
+                preparedStatement.setString(1, userId);
 
-            if (resultSet.next()) {
-                user = new User(resultSet.getString("userId"), resultSet.getString("password"),
+                return preparedStatement;
+            }
+        }, new SimpleRowMapper() {
+            @Override
+            public Object map(ResultSet resultSet) throws SQLException {
+                return new User(resultSet.getString("userId"), resultSet.getString("password"),
                         resultSet.getString("name"), resultSet.getString("email"));
             }
-        } finally {
-            if (resultSet != null) {
-                resultSet.close();
-            }
-
-            if (preparedStatement != null) {
-                preparedStatement.close();
-            }
-
-            if (connection != null) {
-                connection.close();
-            }
-        }
+        });
 
         return user;
     }
