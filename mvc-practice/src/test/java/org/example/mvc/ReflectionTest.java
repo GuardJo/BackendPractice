7acdb0c1d8025aeebc0207cee7f51c7119aa5d64
kyeongho <dagh1218@naver.com>
2022-10-17 00:02:48 +0900

    ReflectionTest 코드리팩토링
    - Controller scan 관련 부분 별도 메소드로 분리

--- mvc-practice/src/test/java/org/example/mvc/ReflectionTest.java
+++ mvc-practice/src/test/java/org/example/mvc/ReflectionTest.java
@@ -5,7 +5,9 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.reflections.Reflections;
 
+import java.lang.annotation.Annotation;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -14,15 +16,23 @@ public class ReflectionTest {
     @DisplayName("Controller 어노테이션이 붙은 클래스들을 찾는다")
     @Test
     void testSearchController() {
-        Reflections reflections = new Reflections("org.example.mvc");
-        Set<Class<?>> controllers = new HashSet<>();
-
-        controllers.addAll(reflections.getTypesAnnotatedWith(Controller.class));
+        Set<Class<?>> controllers = scanController(List.of(Controller.class));
 
         controllers.forEach((controller) -> {
             System.out.println("Controller : " + controller.getName());
         });
 
         assertThat(2).isEqualTo(controllers.size());
+    }
+
+    private Set<Class<?>> scanController(List<Class<? extends Annotation>> annotaionts) {
+        Reflections reflections = new Reflections("org.example.mvc");
+        Set<Class<?>> controllers = new HashSet<>();
+
+        annotaionts.forEach((annotation) -> {
+            controllers.addAll(reflections.getTypesAnnotatedWith(annotation));
+        });
+
+        return controllers;
     }
 }
